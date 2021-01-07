9ea9ec22225a79b1901b72a190e1d0924d358553
huwd <hwd9053@qq.com>
2021-01-07 23:54:08 +0800

    20210107 图

---
+++ 03-图/src/com/mj/graph/Graph.java
@@ -0,0 +1,16 @@
+package com.mj.graph;
+
+public interface Graph<V, E> {
+    int edgesSize();
+    int verticesSize();
+
+    void addVertex(V v);
+
+    void addEdge(V from, V to);
+    void addEdge(V from, V to, E weight);
+
+    void removeVertex(V v);
+    void removeEdge(V from, V to);
+
+    void bfs(V v);
+}
---
+++ 03-图/src/com/mj/graph/ListGraph.java
@@ -0,0 +1,193 @@
+package com.mj.graph;
+
+import java.util.*;
+
+@SuppressWarnings("unchecked")
+public class ListGraph<V, E> implements Graph<V, E>{
+    private final Map<V, Vertex<V, E>> vertices = new HashMap<>();
+    private final Set<Edge<V, E>> edges = new HashSet<>();
+
+    public void print() {
+        vertices.forEach((V v, Vertex<V, E> vertex) -> {
+            System.out.println(v);
+            System.out.println("-------in--------");
+            System.out.println(vertex.inEdges);
+            System.out.println("-------out--------");
+            System.out.println(vertex.outEdges);
+        });
+
+        edges.forEach((Edge<V, E> edge) -> {
+            System.out.println(edge);
+        });
+    }
+
+    @Override
+    public int edgesSize() {
+        return edges.size();
+    }
+
+    @Override
+    public int verticesSize() {
+        return vertices.size();
+    }
+
+    @Override
+    public void addVertex(V v) {
+        if (vertices.containsKey(v)) return;
+        vertices.put(v, new Vertex<>(v));
+    }
+
+    @Override
+    public void addEdge(V from, V to) {
+        addEdge(from, to, null);
+    }
+
+    @Override
+    public void addEdge(V from, V to, E weight) {
+        // 判断from，to顶点是否存在
+        Vertex<V, E> fromVertex = vertices.get(from);
+        if (fromVertex == null) {
+            fromVertex = new Vertex<>(from);
+            vertices.put(from, fromVertex);
+        }
+        Vertex<V, E> toVertex = vertices.get(to);
+        if (toVertex == null) {
+            toVertex = new Vertex<>(to);
+            vertices.put(to, toVertex);
+        }
+
+        Edge<V, E> edge = new Edge<>(fromVertex, toVertex);
+        edge.weight = weight;
+
+        if (fromVertex.outEdges.remove(edge)) {
+            toVertex.inEdges.remove(edge);
+            edges.remove(edge);
+        }
+
+        fromVertex.outEdges.add(edge);
+        toVertex.inEdges.add(edge);
+        edges.add(edge);
+    }
+
+    @Override
+    public void removeVertex(V v) {
+        Vertex<V, E> vertex = vertices.remove(v);
+        if (vertex == null) return;
+
+        // 如果要一边遍历一边删除的话，用迭代器
+        for (Iterator<Edge<V, E>> iterator = vertex.outEdges.iterator(); iterator.hasNext();) {
+            Edge<V, E> edge = iterator.next();
+            edge.to.inEdges.remove(edge);
+            iterator.remove(); // 将当前遍历到的元素从集合中删掉
+            edges.remove(edge);
+        }
+
+        for (Iterator<Edge<V, E>> iterator = vertex.inEdges.iterator(); iterator.hasNext();) {
+            Edge<V, E> edge = iterator.next();
+            edge.from.outEdges.remove(edge);
+            iterator.remove(); // 将当前遍历到的元素从集合中删掉
+            edges.remove(edge);
+        }
+    }
+
+    @Override
+    public void removeEdge(V from, V to) {
+        Vertex<V, E> fromVertex = vertices.get(from);
+        if (fromVertex == null) {
+            return;
+        }
+
+        Vertex<V, E> toVertex = vertices.get(to);
+        if (toVertex == null) {
+            return;
+        }
+
+        Edge<V, E> edge = new Edge<>(fromVertex, toVertex);
+        if (fromVertex.outEdges.remove(edge)) {
+            toVertex.inEdges.remove(edge);
+            edges.remove(edge);
+        }
+
+    }
+
+    @Override
+    public void bfs(V v) {
+        Vertex<V, E> beginVertex = vertices.get(v);
+        if (beginVertex == null) return;
+
+        Set<Vertex<V, E>> visitedVertices = new HashSet<>();
+        Queue<Vertex<V, E>> queue = new LinkedList<>();
+        queue.offer(beginVertex);
+        visitedVertices.add(beginVertex);
+
+        while (!queue.isEmpty()) {
+            Vertex<V, E> vertex = queue.poll();
+            System.out.println(vertex);
+
+            for (Edge<V, E> edge : vertex.outEdges) {
+                if (visitedVertices.contains(edge.to)) continue;
+                queue.offer(edge.to);
+                visitedVertices.add(edge.to);
+            }
+        }
+    }
+
+    private static class Vertex<V, E> {
+        V value;
+        Set<Edge<V, E>> inEdges = new HashSet<>();
+        Set<Edge<V, E>> outEdges = new HashSet<>();
+        public Vertex(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Vertex)) return false;
+            return Objects.equals(value, ((Vertex<V, E>)o).value);
+        }
+
+        @Override
+        public int hashCode() {
+            return value != null ? value.hashCode() : 0;
+        }
+
+        @Override
+        public String toString() {
+            return value == null ? "null" : value.toString();
+        }
+    }
+
+    private static class Edge<V, E> {
+        Vertex<V, E> from;
+        Vertex<V, E> to;
+        E weight;
+
+        public Edge(Vertex<V, E> from, Vertex<V, E> to) {
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Edge)) return false;
+            Edge<V, E> edge = (Edge<V, E>) o;
+            return Objects.equals(from, edge.from) && Objects.equals(to, edge.to);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = from != null ? from.hashCode() : 0;
+            result = 31 * result + (to != null ? to.hashCode() : 0);
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "Edge{" +
+                    "from=" + from +
+                    ", to=" + to +
+                    ", weight=" + weight +
+                    '}';
+        }
+    }
+}
