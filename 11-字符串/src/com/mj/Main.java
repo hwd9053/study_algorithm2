5cebd15ab33de313693911865e66c324e131eef7
huwd <hwd9053@qq.com>
2021-03-08 22:35:10 +0800

    20210308 night

---
+++ 11-字符串/src/com/mj/Main.java
@@ -0,0 +1,12 @@
+package com.mj;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(BruteForce02.indexOf("Hello World", "H"));
+        System.out.println(BruteForce02.indexOf("Hello World", "d"));
+        System.out.println(BruteForce02.indexOf("Hello World", "or"));
+        System.out.println(BruteForce02.indexOf("Hello World", "abc"));
+
+    }
+}
