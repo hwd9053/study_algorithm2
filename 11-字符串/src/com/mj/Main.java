e2476d627e3f5c660a9b2e901dad084ca48fa895
huwd <hwd9053@qq.com>
2021-03-10 23:31:08 +0800

    20210310 night

--- 11-字符串/src/com/mj/Main.java
+++ 11-字符串/src/com/mj/Main.java
@@ -3,10 +3,10 @@ package com.mj;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(BruteForce02.indexOf("Hello World", "H"));
-        System.out.println(BruteForce02.indexOf("Hello World", "d"));
-        System.out.println(BruteForce02.indexOf("Hello World", "or"));
-        System.out.println(BruteForce02.indexOf("Hello World", "abc"));
+        System.out.println(KMP.indexOf("Hello World", "H"));
+        System.out.println(KMP.indexOf("Hello World", "d"));
+        System.out.println(KMP.indexOf("Hello World", "or"));
+        System.out.println(KMP.indexOf("Hello World", "abc"));
 
     }
 }
